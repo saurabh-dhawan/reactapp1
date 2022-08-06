bb44dffb800a9839a74805fdf73291e13919e661
saurabh-dhawan <71413378+saurabh-dhawan@users.noreply.github.com>
2022-08-06 12:23:54 +1200

    10

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { Amplify } from 'aws-amplify';
+import { API } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { listTodos } from './graphql/queries';
+import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-function App({ signOut }) {
+const initialFormState = { name: '', description: '' }
+
+export default function App() {
+  const [Todos, setTodos] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    const apiData = await API.graphql({ query: listTodos });
+    setTodos(apiData.data.listTodos.items);
+  }
+
+  async function createTodo() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createTodoMutation, variables: { input: formData } });
+    setTodos([ ...Todos, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteTodo({ id }) {
+    const newTodosArray = Todos.filter(Todo => Todo.id !== id);
+    setTodos(newTodosArray);
+    await API.graphql({ query: deleteTodoMutation, variables: { input: { id } }});
+  }
+
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>Good Morning - Neil, Rishi and Mummy!!!!!!!!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+     <div className="App">
+      <h1>My Todos App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Todo name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Todo description"
+        value={formData.description}
+      />
+      <button onClick={createTodo}>Create Todo</button>
+      <div style={{marginBottom: 30}}>
+        {
+          Todos.map(Todo => (
+            <div key={Todo.id || Todo.name}>
+              <h2>{Todo.name}</h2>
+              <p>{Todo.description}</p>
+              <button onClick={() => deleteTodo(Todo)}>Delete Todo</button>
+            </div>
+          ))
+        }
+    </div>
+	
+	<div className="styles">
+      <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+	</div>
+	
+    </div>
   );
 }
-
-export default withAuthenticator(App);
